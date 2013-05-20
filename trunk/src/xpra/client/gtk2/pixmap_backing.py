svn2github/Xpra

commit 2a0c44d8050d9b495b883cd9e96f6f55ac2b9bdd

simplify and cleanup backing init: * always use ClientWindowBase make_new_backing * gtk3 simply overrides get_client_window_class * has_alpha only handled on __init__, not after
git-svn-id: http://www.xpra.org/svn/Xpra@3373 3bb7dfac-3a0b-4e04-842a-767bc560f471

trunk/src/xpra/client/gtk2/pixmap_backing.py
@@ -21,11 +21,10 @@ class PixmapBacking(GTK2WindowBacking):
     def __init__(self, wid, w, h, has_alpha):
         GTK2WindowBacking.__init__(self, wid, w, h, has_alpha)
 
-    def init(self, w, h, has_alpha):
+    def init(self, w, h):
         old_backing = self._backing
         assert w<32768 and h<32768, "dimensions too big: %sx%s" % (w, h)
-        self._has_alpha = has_alpha
-        if has_alpha:
+        if self.has_alpha:
             self._backing = gdk.Pixmap(None, w, h, 32)
             screen = self._backing.get_screen()
             rgba = screen.get_rgba_colormap()

trunk/src/xpra/client/gtk2/window_backing.py
@@ -27,6 +27,7 @@ class GTK2WindowBacking(GTKWindowBacking):
 
     def __init__(self, wid, w, h, has_alpha):
         GTKWindowBacking.__init__(self, wid)
+        self.has_alpha = has_alpha
 
     def init(self, w, h, has_alpha):
         raise Exception("override me!")

trunk/src/xpra/client/client_window_base.py
@@ -71,7 +71,6 @@ class ClientWindowBase(object):
     def make_new_backing(self, backing_class, w, h):
         w = max(1, w)
         h = max(1, h)
-        has_alpha = self._metadata.get("has-alpha", False)
         lock = None
         backing = self._backing
         if backing:
@@ -84,12 +83,13 @@ class ClientWindowBase(object):
                 if USE_FAKE_BACKING:
                     from xpra.client.fake_window_backing import FakeBacking
                     bc = FakeBacking
-                self.debug("make_new_backing(%s, %s, %s) effective backing class=%s", backing_class, w, h, bc)
+                has_alpha = self._metadata.get("has-alpha", False)
+                self.debug("make_new_backing(%s, %s, %s) effective backing class=%s, alpha=%s", backing_class, w, h, bc, has_alpha)
                 backing = bc(self._id, w, h, has_alpha)
                 if self._client.mmap_enabled:
                     backing.enable_mmap(self._client.mmap)
-            self.debug("make_new_backing(%s, %s, %s) init with alpha=%s", backing_class, w, h, has_alpha)
-            backing.init(w, h, has_alpha)
+            self.debug("make_new_backing(%s, %s, %s) calling init", backing_class, w, h)
+            backing.init(w, h)
         finally:
             if lock:
                 lock.release()

trunk/src/xpra/client/gl/gl_client_window.py
@@ -50,21 +50,4 @@ class GLClientWindow(ClientWindow):
         ClientWindow.destroy(self)
 
     def new_backing(self, w, h):
-        debug("GL new_backing(%s, %s)", w, h)
-        w = max(2, w)
-        h = max(2, h)
-        has_alpha = self._metadata.get("has-alpha", False)
-        lock = None
-        if self._backing:
-            lock = self._backing._video_decoder_lock
-        try:
-            if lock:
-                lock.acquire()
-            if self._backing is None:
-                self._backing = self.gl_pixmap_backing_class(self._id, w, h, has_alpha)
-                if self._client.supports_mmap:
-                    self._backing.enable_mmap(self._client.mmap)
-            self._backing.init(w, h)
-        finally:
-            if lock:
-                lock.release()
+        self._backing = self.make_new_backing(self.gl_pixmap_backing_class, w, h)

trunk/src/xpra/client/gtk3/client_window.py
@@ -38,8 +38,8 @@ class ClientWindow(GTKClientWindowBase):
         gtk.Window.__init__(self)
         GTKClientWindowBase.init_window(self)
 
-    def new_backing(self, w, h, has_alpha):
-        self._backing = self.make_new_backing(CairoBacking, w, h, has_alpha)
+    def get_client_window_class(self, metadata):
+        return CairoBacking
 
 
     def xget_u32_property(self, target, name):

commit 0089807d2ad428c32596d066e0855598ba367602

fix transparency breakage caused by r3373 and a duplicate flag: _has_alpha vs has_alpha!
git-svn-id: http://www.xpra.org/svn/Xpra@3457 3bb7dfac-3a0b-4e04-842a-767bc560f471

trunk/src/xpra/client/gtk2/pixmap_backing.py
@@ -18,13 +18,10 @@ Backing using a gdk.Pixmap
 """
 class PixmapBacking(GTK2WindowBacking):
 
-    def __init__(self, wid, w, h, has_alpha):
-        GTK2WindowBacking.__init__(self, wid, w, h, has_alpha)
-
     def init(self, w, h):
         old_backing = self._backing
         assert w<32768 and h<32768, "dimensions too big: %sx%s" % (w, h)
-        if self.has_alpha:
+        if self._has_alpha:
             self._backing = gdk.Pixmap(None, w, h, 32)
             screen = self._backing.get_screen()
             rgba = screen.get_rgba_colormap()
@@ -65,10 +62,9 @@ class PixmapBacking(GTK2WindowBacking):
         return True
 
     def _do_paint_rgb32(self, img_data, x, y, width, height, rowstride, options, callbacks):
-        log.debug("do_paint_rgb32(%s bytes, %s, %s, %s, %s, %s, %s, %s)", len(img_data), x, y, width, height, rowstride, options, callbacks)
+        #log.debug("do_paint_rgb32(%s bytes, %s, %s, %s, %s, %s, %s, %s) backing depth=%s", len(img_data), x, y, width, height, rowstride, options, callbacks, self._backing.get_depth())
         #log.info("data head=%s", [hex(ord(v))[2:] for v in list(img_data[:500])])
         pixbuf = gdk.pixbuf_new_from_data(img_data, gtk.gdk.COLORSPACE_RGB, True, 8, width, height, rowstride)
-        log.debug("do_paint_rgb32(..) backing depth=%s", self._backing.get_depth())
         cr = self._backing.cairo_create()
         cr.rectangle(x, y, width, height)
         cr.set_source_pixbuf(pixbuf, x, y)

trunk/src/xpra/client/gtk2/window_backing.py
@@ -26,7 +26,7 @@ class GTK2WindowBacking(GTKWindowBacking):
 
     def __init__(self, wid, w, h, has_alpha):
         GTKWindowBacking.__init__(self, wid)
-        self.has_alpha = has_alpha
+        self._has_alpha = has_alpha
 
     def init(self, w, h, has_alpha):
         raise Exception("override me!")