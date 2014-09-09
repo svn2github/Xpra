a4193884caabb0330bf8cb9c72b8df1809571c11
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2014-09-09 16:09:20 +0000

    r7509 for v0.14.x branch: more reliable fallback for unpremultiply
    git-svn-id: http://www.xpra.org/svn/Xpra@7543 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v0.14.x/src/xpra/client/window_backing_base.py
+++ tags/v0.14.x/src/xpra/client/window_backing_base.py
@@ -161,15 +161,11 @@ class WindowBackingBase(object):
         return img_data
 
     def unpremultiply(self, img_data):
-        if type(img_data)==str:
-            #cannot do in-place:
-            assert unpremultiply_argb is not None, "missing argb.unpremultiply_argb"
+        try:
+            unpremultiply_argb_in_place(img_data)
+            return img_data
+        except:
             return byte_buffer_to_buffer(unpremultiply_argb(img_data))
-        #assume this is a writeable buffer (ie: ctypes from mmap):
-        assert unpremultiply_argb_in_place is not None, "missing argb.unpremultiply_argb_in_place"
-        unpremultiply_argb_in_place(img_data)
-        return img_data
-
 
     def process_delta(self, raw_data, width, height, rowstride, options):
         """
