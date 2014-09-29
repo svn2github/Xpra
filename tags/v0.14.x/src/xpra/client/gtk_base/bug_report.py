e08f43400ffdb476f405ccbbe7e6865ed3e3fb63
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2014-09-29 08:25:13 +0000

    r7823 for v0.14.x branch: fix dpi issues with ms windows and high dpi settings
    git-svn-id: http://www.xpra.org/svn/Xpra@7829 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v0.14.x/src/xpra/client/gtk_base/bug_report.py
+++ tags/v0.14.x/src/xpra/client/gtk_base/bug_report.py
@@ -9,6 +9,9 @@ import os.path
 import sys
 import signal
 
+from xpra.platform.gui import init as gui_init
+gui_init()
+
 from xpra.gtk_common.gobject_compat import import_gtk, import_gdk, import_gobject, import_pango
 
 gtk = import_gtk()
@@ -304,9 +307,8 @@ def main():
     gtk_main_quit_on_fatal_exceptions_enable()
 
     from xpra.platform import init as platform_init
-    from xpra.platform.gui import init as gui_init, ready as gui_ready
+    from xpra.platform.gui import ready as gui_ready
     platform_init("Xpra-Bug-Report", "Xpra Bug Report")
-    gui_init()
 
     #logging init:
     if "-v" in sys.argv:
