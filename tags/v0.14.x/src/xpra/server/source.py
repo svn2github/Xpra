7804dd47523cac4ddb037203a3160d0afdb6bb49
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2014-10-25 09:22:44 +0000

    r7970 + r7972 for v0.14.x branch: avoid errors when very limited encodings are available on the client
    git-svn-id: http://www.xpra.org/svn/Xpra@7978 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v0.14.x/src/xpra/server/source.py
+++ tags/v0.14.x/src/xpra/server/source.py
@@ -21,7 +21,6 @@ cursorlog = Logger("cursor")
 
 from xpra.server import ClientException
 from xpra.server.source_stats import GlobalPerformanceStatistics
-from xpra.server.window_source import WindowSource, HAS_ALPHA
 from xpra.server.window_video_source import WindowVideoSource
 from xpra.server.batch_config import DamageBatchConfig
 from xpra.simple_stats import add_list_stats, std_unit
@@ -544,7 +543,6 @@ class ServerSource(object):
         self.notify_startup_complete = c.boolget("notify-startup-complete")
         self.namespace = c.boolget("namespace")
         self.control_commands = c.strlistget("control_commands")
-        self.supports_transparency = HAS_ALPHA and c.boolget("encoding.transparency")
 
         self.desktop_size = c.intpair("desktop_size")
         if self.desktop_size is not None:
@@ -1518,15 +1516,7 @@ class ServerSource(object):
         ws = self.window_sources.get(wid)
         if ws is None:
             batch_config = self.make_batch_config(wid, window)
-            wclass = WindowSource
-            #don't use video for system trays
-            #or for transparent windows (if the client supports transparency) since video doesn't do alpha (yet?)
-            if not window.is_tray() and (not window.has_alpha() or not self.supports_transparency):
-                wts = get_generic_window_type(window, strip_net=True)
-                if set(("NORMAL", "DIALOG", "DESKTOP")).intersection(wts):
-                    wclass = WindowVideoSource
-
-            ws = wclass(self.idle_add, self.timeout_add, self.source_remove,
+            ws = WindowVideoSource(self.idle_add, self.timeout_add, self.source_remove,
                               self.queue_size, self.queue_damage, self.queue_packet,
                               self.statistics,
                               wid, window, batch_config, self.auto_refresh_delay,
