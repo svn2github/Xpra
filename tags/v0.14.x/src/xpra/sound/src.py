svn2github/Xpra

commit 65c6568d58bff795381f9f9f1bc96d715519bd29

r8154 + r8155 for v0.14.x branch: correctly pass gstreamer signed 64 bit values as -1
git-svn-id: http://www.xpra.org/svn/Xpra@8171 3bb7dfac-3a0b-4e04-842a-767bc560f471

tags/v0.14.x/src/xpra/sound/src.py
@@ -16,6 +16,13 @@ log = Logger("sound")
 
 SOURCES = ["autoaudiosrc"]
 if has_pa():
+
+
+def normv(v):
+    if v==2**64-1:
+        return -1
+    return v
+
     SOURCES.append("pulsesrc")
 if sys.platform.startswith("darwin"):
     SOURCES.append("osxaudiosrc")
@@ -110,8 +117,8 @@ class SoundSource(SoundPipeline):
         #info = sample.get_info()
         size = buf.get_size()
         data = buf.extract_dup(0, size)
-        self.do_emit_buffer(data, {"timestamp"  : buf.pts,
-                                   "duration"   : buf.duration})
+        self.do_emit_buffer(data, {"timestamp"  : normv(buf.pts),
+                                   "duration"   : normv(buf.duration)})
 
 
     def on_new_preroll0(self, appsink):
@@ -134,8 +141,8 @@ class SoundSource(SoundPipeline):
         #            "duration"  : buf.duration,
         #            "offset"    : buf.offset,
         #            "offset_end": buf.offset_end}
-        self.do_emit_buffer(buf.data, {"timestamp" : buf.timestamp,
-                                       "duration"  : buf.duration})
+        self.do_emit_buffer(buf.data, {"timestamp" : normv(buf.timestamp),
+                                       "duration"  : normv(buf.duration)})
 
 
     def do_emit_buffer(self, data, metadata={}):

commit 0775d54b5c5f8faceb031e82061c05691c319e8c

fix patch breakage applied in r8171 - wth?
git-svn-id: http://www.xpra.org/svn/Xpra@8175 3bb7dfac-3a0b-4e04-842a-767bc560f471

tags/v0.14.x/src/xpra/sound/src.py
@@ -14,15 +14,14 @@ from xpra.log import Logger
 log = Logger("sound")
 
 
-SOURCES = ["autoaudiosrc"]
-if has_pa():
-
-
 def normv(v):
     if v==2**64-1:
         return -1
     return v
 
+
+SOURCES = ["autoaudiosrc"]
+if has_pa():
     SOURCES.append("pulsesrc")
 if sys.platform.startswith("darwin"):
     SOURCES.append("osxaudiosrc")