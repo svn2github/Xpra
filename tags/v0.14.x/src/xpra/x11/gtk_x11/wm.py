9bf58823718a496bdf57cdaec75d5c5bcc57b93b
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2014-12-18 06:58:20 +0000

    fix r8075 backport of r8065: we must use python2.4 compatible syntax in the 0.14.x branch
    git-svn-id: http://www.xpra.org/svn/Xpra@8251 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v0.14.x/src/xpra/x11/gtk_x11/wm.py
+++ tags/v0.14.x/src/xpra/x11/gtk_x11/wm.py
@@ -446,7 +446,7 @@ class Wm(gobject.GObject):
     def get_net_wm_name(self):
         try:
             return prop_get(self._ewmh_window, "_NET_WM_NAME", "utf8", ignore_errors=False, raise_xerrors=False)
-        except Exception as e:
+        except Exception, e:
             log.error("error querying _NET_WM_NAME: %s", e)
 
 gobject.type_register(Wm)
