569ae9b90a96c46e4bf90bd7825310808a48ba35
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2015-01-17 06:05:35 +0000

    r8486 for v0.14.x branch: don't iconify unmapped windows
    git-svn-id: http://www.xpra.org/svn/Xpra@8487 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v0.14.x/src/xpra/x11/server.py
+++ tags/v0.14.x/src/xpra/x11/server.py
@@ -657,6 +657,7 @@ class XpraServer(gobject.GObject, X11ServerBase):
 
     def _process_unmap_window(self, proto, packet):
         wid = packet[1]
+        iconified = bool(packet[2])
         window = self._id_to_window.get(wid)
         if not window:
             log("cannot map window %s: already removed!", wid)
@@ -665,7 +666,7 @@ class XpraServer(gobject.GObject, X11ServerBase):
         windowlog("client unmapped window %s - %s", wid, window)
         for ss in self._server_sources.values():
             ss.unmap_window(wid, window)
-        if not window.get_property("iconic"):
+        if iconified and not window.get_property("iconic"):
             window.set_property("iconic", True)
         self._desktop_manager.hide_window(window)
 
