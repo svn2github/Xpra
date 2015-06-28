f67bb1e639547bca07fc2c153827e6549aa61a68
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2015-06-28 13:22:51 +0000

    once again, patch made a mess - fixup for r9677
    git-svn-id: http://www.xpra.org/svn/Xpra@9758 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v0.14.x/src/xpra/x11/server.py
+++ tags/v0.14.x/src/xpra/x11/server.py
@@ -392,7 +392,7 @@ class XpraServer(gobject.GObject, X11ServerBase):
 
     def _add_new_window_common(self, window):
         windowlog("adding window %s", window)
-        for prop in window.get_dynamic_property_names():
+        for prop in window._window_export_properties:
             window.connect("notify::%s" % prop, self._update_metadata)
         wid = X11ServerBase._add_new_window_common(self, window)
         window.managed_connect("client-contents-changed", self._contents_changed)
