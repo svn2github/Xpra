2abb852d164e0b9b7e41387928980df3765b423d
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2015-07-07 13:13:49 +0000

    r9820 + r9881 fixup for v0.15.x branch: log internal setup failures
    git-svn-id: http://www.xpra.org/svn/Xpra@9882 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v0.15.x/src/xpra/x11/gtk_x11/wm.py
+++ tags/v0.15.x/src/xpra/x11/gtk_x11/wm.py
@@ -326,7 +326,7 @@ class Wm(gobject.GObject):
             with xsync:
                 self.do_manage_client(gdkwindow)
         except Exception as e:
-            if LOG_MANAGE_FAILURES:
+            if LOG_MANAGE_FAILURES or type(e) not in (Unmanageable, ):
                 l = log.warn
             else:
                 l = log
