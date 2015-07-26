90e5de88666debf8c89f09445c341a4b38d16f3a
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2015-07-26 06:07:07 +0000

    make lz4 version parsing and checking more robust and future proof
    git-svn-id: http://www.xpra.org/svn/Xpra@10042 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v0.14.x/src/xpra/net/compression.py
+++ tags/v0.14.x/src/xpra/net/compression.py
@@ -26,9 +26,7 @@ try:
         return level | LZ4_FLAG, LZ4_compress(packet)
     #try to figure out the version number:
     if hasattr(lz4, "VERSION"):
-        lz4_version = lz4.VERSION
-        if hasattr(lz4, "LZ4_VERSION"):
-            lz4_version.append(lz4.LZ4_VERSION)
+        lz4_version = str(lz4.VERSION)
     elif hasattr(lz4, "__file__"):
         #hack it..
         import os.path
@@ -49,8 +47,10 @@ try:
                 #we want at least two numbers first:
                 if len(tmpv)>=2:
                     #ie: (0, 7, 0)
-                    lz4_version = tuple(tmpv)
-                    assert lz4_version>=(0, 7), "versions older than 0.7.0 are vulnerable and should not be used, see CVE-2014-4715"
+                    lz4_version = ".".join(tmpv)
+    assert lz4_version>="0.7", "versions older than 0.7.0 are vulnerable and should not be used, see CVE-2014-4715"
+    if hasattr(lz4, "LZ4_VERSION"):
+        lz4_version += "."+str(lz4.LZ4_VERSION)
 except Exception, e:
     log("lz4 not found: %s", e)
     LZ4_uncompress = None
