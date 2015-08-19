f82e2a103062e5d76826f49ebc15f230bd4e863e
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2015-08-19 03:48:33 +0000

    r10310 for v0.15.x branch: fix the virtual desktop naming code
    git-svn-id: http://www.xpra.org/svn/Xpra@10338 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v0.15.x/src/xpra/server/server_base.py
+++ tags/v0.15.x/src/xpra/server/server_base.py
@@ -1731,7 +1731,8 @@ class ServerBase(ServerCore):
                 name = "Main"
             else:
                 name = "Desktop %s" % (i+1)
-            if i<len(ss.desktop_names) and ss.desktop_names[i]:
+            for ss in self._server_sources.values():
+                if ss.desktops and i<len(ss.desktop_names) and ss.desktop_names[i]:
                 name = ss.desktop_names[i]
             names.append(name)
         self.set_desktops(names)
