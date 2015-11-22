5665f465cf9edfe7572e9f5bb77fce50603e85a7
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2015-11-22 08:04:42 +0000

    r11304 for v0.15.x branch: forward configure requests wrongly sent to the root window
    git-svn-id: http://www.xpra.org/svn/Xpra@11309 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v0.15.x/src/xpra/x11/gtk_x11/wm.py
+++ tags/v0.15.x/src/xpra/x11/gtk_x11/wm.py
@@ -418,6 +418,8 @@ class Wm(gobject.GObject):
         # accurate info on what the app is actually requesting.
         log("do_child_configure_request_event(%s)", event)
         if event.window in self._windows:
+            log("do_child_configure_request_event(%s) value_mask=%s, forwarding to %s", event, configure_bits(event.value_mask), model)
+            model.do_child_configure_request_event(event)
             return
         log("Reconfigure on withdrawn window")
         with xswallow:
