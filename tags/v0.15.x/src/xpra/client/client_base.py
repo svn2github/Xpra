2dde32038fa7e236e1ce264b3a0b0a59ca33aa7d
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2016-02-29 14:53:07 +0000

    r12057 (partial) for v0.15.x and v0.16.x: don't cause warnings when forwarding large log packets
    git-svn-id: http://www.xpra.org/svn/Xpra@12106 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v0.15.x/src/xpra/client/client_base.py
+++ tags/v0.15.x/src/xpra/client/client_base.py
@@ -210,6 +210,7 @@ class XpraClientBase(object):
         self._protocol = Protocol(self.get_scheduler(), conn, self.process_packet, self.next_packet)
         self._protocol.large_packets.append("keymap-changed")
         self._protocol.large_packets.append("server-settings")
+        self._protocol.large_packets.append("logging")
         self._protocol.set_compression_level(self.compression_level)
         self._protocol.receive_aliases.update(self._aliases)
         self._protocol.enable_default_encoder()
