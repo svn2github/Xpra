e5aaeb3d973d2f00852c13e9ffe5cdc07ce2c08b
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2016-08-06 07:40:04 +0000

    r13198 + r13202 for v0.17.x branch: avoid errors if there is no cursor data to send
    git-svn-id: http://www.xpra.org/svn/Xpra@13245 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v0.17.x/src/xpra/server/source.py
+++ tags/v0.17.x/src/xpra/server/source.py
@@ -1557,8 +1557,9 @@ class ServerSource(object):
             return
         def do_send_cursor():
             self.send_cursor_pending = False
-            cursor_data, cursor_sizes = self.get_cursor_data_cb()
-            if cursor_data:
+            cd = self.get_cursor_data_cb()
+            if cd and cd[0]:
+                cursor_data, cursor_sizes = cd
                 if self.last_cursor_sent and self.last_cursor_sent==cursor_data[:8]:
                     cursorlog("do_send_cursor(..) cursor identical to the last one we sent, nothing to do")
                     return
