svn2github/Xpra

commit b0d9492c36964e996c802426c4689852fb7d024d

* add "--depth" option to make it easier to test different display bit depths (#1445, #1315, #909, #1309) * pillow can convert r210 before encoding, so remove duplicated code from window source * make sure we select the right encoding in fallback cases (scrolling detection / video) * use video fallback for any other bit depth than 24 (all the video codecs we currently have only handle 24bpp)
git-svn-id: http://www.xpra.org/svn/Xpra@15124 3bb7dfac-3a0b-4e04-842a-767bc560f471

trunk/src/xpra/codecs/pillow/encode.py
@@ -53,6 +53,7 @@ def get_info():
 
 
 def encode(coding, image, quality, speed, supports_transparency):
+    log("pillow.encode%s", (coding, image, quality, speed, supports_transparency))
     pixel_format = image.get_pixel_format()
     palette = None
     w = image.get_width()
@@ -61,8 +62,10 @@ def encode(coding, image, quality, speed, supports_transparency):
         "RLE8"  : "P",
         "XRGB"  : "RGB",
         "BGRX"  : "RGB",
+        "RGBX"  : "RGBX",
         "RGBA"  : "RGBA",
         "BGRA"  : "RGBA",
+        "BGRX"  : "RGBA",
         }.get(pixel_format, pixel_format)
     bpp = 32
     #remove transparency if it cannot be handled:

trunk/src/xpra/scripts/config.py
@@ -516,6 +516,7 @@ OPTION_TYPES = {
                     "idle-timeout"      : int,
                     "server-idle-timeout" : int,
                     "sync-xvfb"         : int,
+                    "pixel-depth"       : int,
                     #float options:
                     "auto-refresh-delay": float,
                     #boolean options:
@@ -756,6 +757,7 @@ def get_defaults():
                     "idle-timeout"      : 0,
                     "server-idle-timeout" : 0,
                     "sync-xvfb"         : 0,
+                    "pixel-depth"       : 24,
                     "auto-refresh-delay": 0.15,
                     "daemon"            : CAN_DAEMONIZE,
                     "use-display"       : False,

trunk/src/xpra/scripts/main.py
@@ -810,6 +810,9 @@ def do_parse_cmdline(cmdline, defaults):
     group.add_option("--dpi", action="store",
                       dest="dpi", default=defaults.dpi,
                       help="The 'dots per inch' value that client applications should try to honour, from 10 to 1000 or 0 for automatic setting. Default: %s." % print_number(defaults.dpi))
+    group.add_option("--pixel-depth", action="store",
+                      dest="pixel_depth", default=defaults.pixel_depth,
+                      help="The bits per pixel of the virtual framebuffer (8, 16, 24 or 30). Default: %s." % defaults.pixel_depth)
     group.add_option("--sync-xvfb", action="store",
                       dest="sync_xvfb", default=defaults.sync_xvfb,
                       help="How often to synchronize the virtual framebuffer used for X11 seamless servers (0 to disable). Default: %s." % defaults.sync_xvfb)

trunk/src/xpra/scripts/server.py
@@ -792,7 +792,7 @@ def close_fds(excluding=[0, 1, 2]):
             except:
                 pass
 
-def start_Xvfb(xvfb_str, display_name, cwd):
+def start_Xvfb(xvfb_str, pixel_depth, display_name, cwd):
     if os.name!="posix":
         raise InitException("starting an Xvfb is not supported on %s" % os.name)
     if not xvfb_str:
@@ -850,6 +850,9 @@ def start_Xvfb(xvfb_str, display_name, cwd):
     if not xvfb_cmd:
         raise InitException("cannot start Xvfb, the command definition is missing!")
     xvfb_executable = xvfb_cmd[0]
+    if (xvfb_executable.endswith("Xorg") or xvfb_executable.endswith("Xdummy")) and pixel_depth>0:
+        xvfb_cmd.append("-depth")
+        xvfb_cmd.append(str(pixel_depth))
     if use_display_fd:
         # 'S' means that we allocate the display automatically
         r_pipe, w_pipe = os.pipe()
@@ -858,6 +861,7 @@ def start_Xvfb(xvfb_str, display_name, cwd):
         def preexec():
             setsid()
             close_fds([0, 1, 2, r_pipe, w_pipe])
+        #print("xvfb_cmd=%s" % (xvfb_cmd, ))
         xvfb = subprocess.Popen(xvfb_cmd, executable=xvfb_executable, close_fds=False,
                                 stdin=subprocess.PIPE, preexec_fn=preexec, cwd=cwd)
         # Read the display number from the pipe we gave to Xvfb
@@ -1268,7 +1272,15 @@ def run_server(error_cb, opts, mode, xpra_file, extra_args, desktop_display=None
     if start_vfb:
         assert not proxying
         try:
-            xvfb, display_name, xauth_data = start_Xvfb(opts.xvfb, display_name, cwd)
+            pixel_depth = int(opts.pixel_depth)
+        except ValueError as e:
+            raise InitException("invalid value '%s' for pixel depth, must be a number" % opts.pixel_depth)
+        if pixel_depth not in (8, 16, 24, 30):
+            raise InitException("invalid pixel depth: %s (%s)" % pixel_depth)
+        if not starting_desktop and pixel_depth==8:
+            raise InitException("pixel depth 8 is only supported in 'start-desktop' mode")
+        try:
+            xvfb, display_name, xauth_data = start_Xvfb(opts.xvfb, pixel_depth, display_name, cwd)
         except OSError as e:
             log.error("Error starting Xvfb:")
             log.error(" %s", e)

trunk/src/xpra/server/window/window_source.py
@@ -1939,7 +1939,6 @@ class WindowSource(object):
         #for more information on pixel formats supported by PIL / Pillow, see:
         #https://github.com/python-imaging/Pillow/blob/master/libImaging/Unpack.c
         assert coding in self.server_core_encodings
-        self.no_r210(image, ["RGBA", "RGBX"])
         q = options.get("quality") or self.get_quality(coding)
         s = options.get("speed") or self.get_speed(coding)
         transparency = self.supports_transparency and options.get("transparency", True)

trunk/src/xpra/server/window/window_video_source.py
@@ -1532,6 +1532,10 @@ class WindowVideoSource(WindowSource):
     def get_video_fallback_encoding(self, order=PREFERED_ENCODING_ORDER):
         #find one that is not video:
         fallback_encodings = [x for x in order if (x in self.non_video_encodings and x in self._encoders and x!="mmap")]
+        if self.image_depth==8:
+            return "png/P"
+        elif self.image_depth=="30":
+            return "rgb"
         if not fallback_encodings:
             if not self.is_cancelled():
                 log.warn("Warning: no non-video fallback encodings are available!")
@@ -1621,7 +1625,7 @@ class WindowVideoSource(WindowSource):
                 except Exception:
                     scrolllog.error("Error during scrolling detection")
                     scrolllog.error(" with image=%s, options=%s", image, options, exc_info=True)
-        if not self.common_video_encodings or self.image_depth<=16:
+        if not self.common_video_encodings or self.image_depth!=24:
             #we have to send using a non-video encoding as that's all we have!
             return self.video_fallback(image, options)
 

commit 7e53a827faf1f65b95a87d9dbe6b765ca50c18e9

#1446: fix breakage introduced in r15124: BGRX cannot be converted to RGBA
git-svn-id: http://www.xpra.org/svn/Xpra@15128 3bb7dfac-3a0b-4e04-842a-767bc560f471

trunk/src/xpra/codecs/pillow/encode.py
@@ -60,7 +60,6 @@ def encode(coding, image, quality, speed, supports_transparency):
         "RGBX"  : "RGBX",
         "RGBA"  : "RGBA",
         "BGRA"  : "RGBA",
-        "BGRX"  : "RGBA",
         }.get(pixel_format, pixel_format)
     bpp = 32
     #remove transparency if it cannot be handled,