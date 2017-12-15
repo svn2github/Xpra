d347e96b8c205391ba40d586bb09cc91bea09102
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2017-12-15 04:24:23 +0000

    r17622 + r17638 + r17639: don't underestimate available bandwidth
    git-svn-id: http://www.xpra.org/svn/Xpra@17645 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v2.2.x/src/xpra/server/window/window_source.py
+++ tags/v2.2.x/src/xpra/server/window/window_source.py
@@ -39,6 +39,7 @@ MAX_DELTA_SIZE = envint("XPRA_MAX_DELTA_SIZE", 32768)
 MAX_DELTA_HITS = envint("XPRA_MAX_DELTA_HITS", 20)
 MIN_WINDOW_REGION_SIZE = envint("XPRA_MIN_WINDOW_REGION_SIZE", 1024)
 MAX_SOFT_EXPIRED = envint("XPRA_MAX_SOFT_EXPIRED", 5)
+BANDWIDTH_DETECTION = envbool("XPRA_BANDWIDTH_DETECTION", True)
 
 HAS_ALPHA = envbool("XPRA_ALPHA", True)
 FORCE_BATCH = envint("XPRA_FORCE_BATCH", False)
@@ -1855,7 +1856,7 @@ class WindowSource(object):
             elapsed_ms = int((now-ack_pending[0])*1000)
             #if this packet completed late, record congestion send speed:
             if elapsed_ms>max_send_delay and ldata>1024:
-                self.record_congestion_event((elapsed_ms*100/max_send_delay)-100)
+                self.record_congestion_event((elapsed_ms*100/max_send_delay)-100, ldata, elapsed_ms)
             self.schedule_auto_refresh(packet, options)
         if process_damage_time>0:
             now = monotonic_time()
@@ -1873,33 +1874,43 @@ class WindowSource(object):
             self.idle_add(self.damage, x, y, width, height)
         return resend
 
-    def record_congestion_event(self, late_pct):
-        gs = self.global_statistics
-        if not gs or len(gs.bytes_sent)<5:
+    def record_congestion_event(self, late_pct, ldata, elapsed_ms):
+        if not BANDWIDTH_DETECTION:
             return
-        now = monotonic_time()
-        #find a sample more than a second old
-        #(hopefully before the congestion started)
-        for i in range(1,4):
-            stime1, svalue1 = gs.bytes_sent[-i]
-            if now-stime1>1:
-                break
-        i += 1
-        #find a sample more than 4 seconds earlier,
-        #with at least 2KB sent in between:
-        while i<len(gs.bytes_sent):
-            stime2, svalue2 = gs.bytes_sent[-i]
-            if stime1-stime2>4 and (svalue1-svalue2)>2000:
-                break
+        #calculate the send speed for the packet we just sent:
+        last_send_speed = int(ldata*8*1000/elapsed_ms)
+        send_speed = last_send_speed
+        avg_send_speed = 0
+        gs = self.global_statistics
+        if gs and len(gs.bytes_sent)>=5:
+            now = monotonic_time()
+            #find a sample more than a second old
+            #(hopefully before the congestion started)
+            for i in range(1,4):
+                stime1, svalue1 = gs.bytes_sent[-i]
+                if now-stime1>1:
+                    break
             i += 1
-        #calculate the send speed over that interval:
-        bcount = svalue1-svalue2
-        t = stime1-stime2
-        if t>0 and t<10:
-            send_speed = int(bcount*8/t)
-            statslog("record_congestion_event(%i) %iKbps", late_pct, send_speed/1024)
-            gs.congestion_send_speed.append((now, late_pct, send_speed))
-            gs.last_congestion_time = now
+            #find a sample more than 4 seconds earlier,
+            #with at least 64KB sent in between:
+            t = 0
+            while i<len(gs.bytes_sent):
+                stime2, svalue2 = gs.bytes_sent[-i]
+                t = stime1-stime2
+                if t>10:
+                    #too far back, not enough data sent in 10 seconds
+                    break
+                if t>=4 and (svalue1-svalue2)>=65536:
+                    break
+                i += 1
+            if t>=4 and t<=10:
+                #calculate the send speed over that interval:
+                bcount = svalue1-svalue2
+                avg_send_speed = int(bcount*8/t)
+                send_speed = (avg_send_speed + last_send_speed)//2
+        statslog("record_congestion_event(%i, %i, %ims) %iKbps (average=%iKbps, last packet=%iKbps)", late_pct, ldata, elapsed_ms, send_speed//1024, avg_send_speed//1024, last_send_speed//1024)
+        gs.congestion_send_speed.append((now, late_pct, send_speed))
+        gs.last_congestion_time = now
 
     def damage_packet_acked(self, damage_packet_sequence, width, height, decode_time, message):
         """
