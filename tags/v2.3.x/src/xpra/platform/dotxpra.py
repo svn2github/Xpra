cf1d84deb88a22f309908405b7b81872d714d842
antoine <antoine@3bb7dfac-3a0b-4e04-842a-767bc560f471>
2018-09-20 11:45:24 +0000

    r20450 backport: fix potential file descriptor leak
    git-svn-id: http://www.xpra.org/svn/Xpra@20480 3bb7dfac-3a0b-4e04-842a-767bc560f471

--- tags/v2.3.x/src/xpra/platform/dotxpra.py
+++ tags/v2.3.x/src/xpra/platform/dotxpra.py
@@ -80,6 +80,7 @@ class DotXpra(object):
         sock.settimeout(timeout)
         try:
             sock.connect(sockpath)
+            return self.LIVE
         except socket.error as e:
             debug("get_server_state: connect(%s)=%s", sockpath, e)
             err = e.args[0]
@@ -90,10 +91,12 @@ class DotXpra(object):
                 return self.UNKNOWN
             if err in (errno.EWOULDBLOCK, errno.ENOENT):
                 return self.DEAD
-        else:
-            sock.close()
-            return self.LIVE
-        return self.UNKNOWN
+            return self.UNKNOWN
+        finally:
+            try:
+                sock.close()
+            except:
+                debug("%s.close()", sock, exc_info=True)
 
 
     def displays(self, check_uid=0, matching_state=None):
