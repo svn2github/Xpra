svn2github/Xpra

commit 19182d2335a4554cb68dbd31854bf88f5285d3cf

we only have a single module per name, so don't bother using lists
git-svn-id: http://www.xpra.org/svn/Xpra@21210 3bb7dfac-3a0b-4e04-842a-767bc560f471

trunk/src/xpra/codecs/video_helper.py
@@ -17,15 +17,15 @@ from xpra.util import csv, engs
 #the codec loader uses the names...
 #but we need the module name to be able to probe without loading the codec:
 CODEC_TO_MODULE = {
-                   "vpx"        : ["vpx"],
-                   "x264"       : ["enc_x264"],
-                   "x265"       : ["enc_x265"],
-                   "nvenc"      : ["nvenc"],
-                   "swscale"    : ["csc_swscale"],
-                   "libyuv"     : ["csc_libyuv"],
-                   "avcodec2"   : ["dec_avcodec2"],
-                   "ffmpeg"     : ["enc_ffmpeg"],
-                   }
+    "vpx"        : "vpx",
+    "x264"       : "enc_x264",
+    "x265"       : "enc_x265",
+    "nvenc"      : "nvenc",
+    "swscale"    : "csc_swscale",
+    "libyuv"     : "csc_libyuv",
+    "avcodec2"   : "dec_avcodec2",
+    "ffmpeg"     : "enc_ffmpeg",
+    }
 
 def has_codec_module(module_name):
     top_module = "xpra.codecs.%s" % module_name
@@ -40,11 +40,10 @@ def has_codec_module(module_name):
 def try_import_modules(codec_names):
     names = []
     for codec_name in codec_names:
-        module_names = CODEC_TO_MODULE[codec_name]
-        for module_name in module_names:
-            if has_codec_module(module_name):
-                names.append(codec_name)
-                break
+        module_name = CODEC_TO_MODULE[codec_name]
+        if has_codec_module(module_name):
+            names.append(codec_name)
+            break
     return names
 
 #all the codecs we know about:

commit 3a793f5f5a924b1d3eae6b95356b1210e1c00d63

#2082: fix breakage introduced in r21210
git-svn-id: http://www.xpra.org/svn/Xpra@21229 3bb7dfac-3a0b-4e04-842a-767bc560f471

trunk/src/xpra/codecs/video_helper.py
@@ -43,7 +43,6 @@ def try_import_modules(codec_names):
         module_name = CODEC_TO_MODULE[codec_name]
         if has_codec_module(module_name):
             names.append(codec_name)
-            break
     return names
 
 #all the codecs we know about: